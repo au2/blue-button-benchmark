07159fc7469cacba3dfcb2cdb5802156e031df6f
au2 <afsin@amida-tech.com>
2014-08-01 14:13:53 -0400

    first version of full patient generateRecord

--- test/test-gendata.js
+++ test/test-gendata.js
@@ -1,18 +1,18 @@
 "use strict"
 
-var bbhelper = require('../lib/bbhelper');
+var dg = require('../lib/datagenerator');
 var hdc = require('../lib/healthdatacontent');
 
 describe('generate data', function() {
     it('generate section', function(done) {
         var options = {
             num_sections: 3,
+            num_entries: 3,
 
             array_size: 3,
             string_size: 7
         };
-        var schema = hdc.getSchema('allergies');
-        var result = bbhelper.generateSection(schema, options);
+        var result = dg.generateRecord(options);
         console.log(JSON.stringify(result, undefined, 2));
         done();
     });
